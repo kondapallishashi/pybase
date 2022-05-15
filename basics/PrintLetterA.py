276e283696ab3ccd453d8e9d177727814646c5b9
KONDAPALLI\shashi <kondapallishashi@gmail.com>
2022-05-15 12:45:04 +0530

    Adding a couple more print examples

---
+++ basics/PrintLetterA.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 15 12:42:06 2022
+
+@author: shashi
+"""
+
+print('    *')
+print('   * *')
+print('  *****')
+print(' *     *')
+print('*       *')
---
+++ basics/PrintTriangle.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 15 12:38:53 2022
+
+@author: shashi
+"""
+
+#1 printing a right angle triangle
+print('*')
+print('**')
+print('***')
+print('****')
